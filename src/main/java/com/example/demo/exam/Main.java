39c1ceef89fd702beb22d85e23829aaa10b97878
EugeneTsui <2276290611@qq.com>
2022-04-12 17:32:15 +0800

    Init

---
+++ src/main/java/com/example/demo/exam/Main.java
@@ -0,0 +1,11 @@
+package com.example.demo.exam;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        int num=5;
+        num+=2;
+        System.out.println(num);
+        System.out.println(num+5);
+    }
+}
